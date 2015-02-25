7e290c5e110fab7e13e4acff955993343a0be56e
mini <jonathangildea@gmail.com>
2015-02-25 12:19:47 +0000

    add simple output change

---
+++ src/com/Main.java
@@ -0,0 +1,21 @@
+package com;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		SalesData mySalesData = new SalesData();
+		
+		mySalesData.display();
+		displayGreeting();
+		
+	}
+	
+	static void displayGreeting(){
+		System.out.println("Hello happy sales people");
+		System.out.println("this app shows sales data");
+		
+		System.out.println("Test 1");
+	}
+
+}
